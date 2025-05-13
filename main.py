6a0115cb73fcbb127d6571b737bb336d496604f8
MaksimNikiforov <145759459+fblfca@users.noreply.github.com>
2025-05-13 22:22:57 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.api import endpoints
-
-# Создаём приложение FastAPI
-app = FastAPI(title="Travelling Salesman Problem API")
-
-# Подключаем маршруты (эндпоинты) из файла endpoints
-app.include_router(endpoints.router)
