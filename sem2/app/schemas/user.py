5a3b5b0b33b5ae0356307f44f19708d0cc847f85
MaksimNikiforov <145759459+fblfca@users.noreply.github.com>
2025-05-14 15:22:47 +0300

    Update user.py

--- sem2/app/schemas/user.py
+++ sem2/app/schemas/user.py
@@ -1,23 +1,40 @@
-from pydantic import BaseModel, EmailStr
-
-# Схема для запроса регистрации и входа
-class UserCreate(BaseModel):
-    email: EmailStr  # Валидирует, что это корректный email
-    password: str
-
+# from pydantic import BaseModel, EmailStr
+#
+# # Схема для запроса регистрации и входа
+# class UserCreate(BaseModel):
+#     email: EmailStr  # Валидирует, что это корректный email
+#     password: str
+#
 # # Схема для ответа с данными пользователя
-# class UserResponse(BaseModel):
+# class UserLoginResponse(BaseModel):
 #     id: int
 #     email: str
 #     token: str
 #
 #     class Config:
 #         from_attributes = True  # Позволяет преобразовывать объекты SQLAlchemy в Pydantic
+#
+# # Схема для получения текущего пользователя
+# class UserMe(BaseModel):
+#     id: int
+#     email: str
+#
+#     class Config:
+#         from_attributes = True
+from pydantic import BaseModel
 
-# Схема для получения текущего пользователя
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
 class UserMe(BaseModel):
     id: int
     email: str
 
     class Config:
         from_attributes = True
+
+class UserLoginResponse(BaseModel):
+    id: int
+    email: str
+    access_token: str
